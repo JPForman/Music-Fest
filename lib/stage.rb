24d09b78cd7f8766e51630581b45fa23437701f4
Joshua Forman <cyborgforman@gmail.com>
2019-12-18 14:57:53 -0800

    create tests and working code for Stage CRUD

---
+++ lib/stage.rb
@@ -0,0 +1,48 @@
+class Stage
+
+  @@stages = {}
+  @@total_rows = 0
+
+  attr_reader :name, :genre, :id
+
+  def initialize (name, genre, id)
+    @name = name
+    @genre = genre
+    @id = id || @@total_rows += 1
+  end
+
+  def ==(other_stage)
+    self.name.eql?(other_stage.name)
+    self.genre.eql?(other_stage.genre)
+  end
+
+  def self.clear
+    @@stages = {}
+  end
+
+  def self.all
+    @@stages.values
+  end
+
+  def save()
+    @@stages[self.id] = Stage.new(self.name, self.genre, self.id)
+  end
+
+  def self.find(id)
+    @@stages[id]
+  end
+
+  def update(name, genre)
+    @name = (name == '') ? self.name : name
+    @genre = (genre == '') ? self.genre : genre
+  end
+
+  def delete
+    @@stages.delete(self.id)
+  end
+
+  def self.search(query)
+    Stage.all.select { |stage| stage.name.match?(/(#{query})/i)}
+  end
+
+end
--- spec/stage_spec.rb
+++ spec/stage_spec.rb
@@ -1,21 +1,67 @@
-require('rspec')
 require('stage')
+require('rspec')
 
 
-describe ('.Stage')
+describe('.Stage') do
 
   before(:each) do
-    @stage1 = Stage.new('Stage 1', 'Rock', nil)
-    @stage2 = Stage.new('Stage 2', 'Hip Hop', nil)
+    @stage1 = Stage.new('Shred Factory', 'Rock', nil)
+    @stage2 = Stage.new('Beat Shop', 'Hip Hop', nil)
+    @stage1.save
+    @stage2.save
   end
 
   after(:each) do
-    Album.clear
+    Stage.clear
   end
 
   describe ('#==') do
     it('is the same stage if it hs the same attributes') do
-    stage3 = Stage.new('Stage 1', 'Rock', nil)
-    expect(@stage1).eq(stage3)
+      stage3 = Stage.new('Stage 1', 'Rock', nil)
+      expect(@stage1).to(eq(stage3))
+    end
   end
+
+  describe ('#save') do
+    it('saves a stage') do
+      expect(Stage.all).to(eq([@stage1, @stage2]))
+    end
+  end
+
+  describe ('#clear') do
+    it('clears all stages') do
+      Stage.clear
+      expect(Stage.all).to(eq([]))
+    end
+  end
+
+  describe ('#find') do
+    it('finds a stage by id') do
+      expect(Stage.find(@stage1.id)).to(eq(@stage1))
+    end
+  end
+
+  describe ('#update') do
+    it('updates values of a stage') do
+      @stage1.update("Rockers", "")
+      expect(@stage1.name).to(eq('Rockers'))
+      expect(@stage1.genre).to(eq('Rock'))
+    end
+  end
+
+  describe ('#delete') do
+    it('deletes a stage') do
+      @stage1.delete
+      expect(Stage.all).to(eq([@stage2]))
+    end
+  end
+
+  describe ('#search') do
+    it('returns an array of stages by partial match name') do
+      stage3 = Stage.new("Fape Bounce", "Electronica", nil)
+      stage3.save
+      expect(Stage.search('Fa')).to(eq([@stage1, stage3]))
+    end
+  end
+
 end
